3f7b5100d08a5d2a3cbcfb329d8d5f15ff327b95
aoao72 <plus.96tu@gmail.com>
2024-03-17 17:15:54 +0900

    first commit

---
+++ .history/src/App_20240317165719.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    return (
+      <div>
+        <NameText name="Jack">
+        <Profile age={22} country="US">
+        <NameText name="Mary">
+        <Profile age={20} country="UK">
+      </div>
+    )
+  );
+}
+
+export default App;
---
+++ .history/src/App_20240317165812.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <NameText name="Jack">
+    <Profile age={22} country="US">
+    <NameText name="Mary">
+    <Profile age={20} country="UK">
+  </div>
+  );
+}
+
+export default App;
